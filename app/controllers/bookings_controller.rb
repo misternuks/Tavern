6bb1dffcf536951de1041e1b29ab61ab711715f7
Connor Minto <caminto94@gmail.com>
2023-05-18 18:44:07 +0900

    Fixed the logic of counting bookings and seeing bookings made of you by other users

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,8 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = policy_scope(Booking)
+    @user_bookings = current_user.bookings
+    @dm_bookings = current_user.dm_profile.present? ? @bookings.select{|booking| booking.dm_profile_id == current_user.dm_profile.id} : []
   end
 
   def show
